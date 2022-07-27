171355e3ea24aaff0afea825c22852e4a528dcfc
RexiJoe <rexijoe10@gmail.com>
2022-07-27 09:44:48 -0400

    Agrega algunas modificaciones

---
+++ JSmenuDesplegable.js
@@ -0,0 +1,12 @@
+let iconMenu = document.getElementById("icon-menu");
+let nav = document.getElementById("nav");
+
+const hideMenu = ()=>{
+    nav.classList.toggle("nav-hide")
+}
+
+iconMenu.addEventListener("click",hideMenu)
+
+
+
+
---
+++ JSvalores.js
@@ -0,0 +1,5 @@
+let sistemToken = 1; //este token de sistema equivale a 1 dolar como valor predeterminado
+let valor1 = 3000; //como ejemplo estos son pesos colombianos
+let valor2 = 60; //como ejemplo estos son bolivares
+
+let array = []
